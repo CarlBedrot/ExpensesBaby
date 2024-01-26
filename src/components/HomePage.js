099b9281ec46b1317e6c0102767c7e5555350f51
CarlBedrot <ca1171be-s@student.lu.se>
2024-01-26 17:42:42 +0100

    Added clickable images

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,6 +8,7 @@ import Goals from '../images/Goals.png';
 import Statistics from '../images/statistics.png';
 import emailjs from 'emailjs-com';
 import { IoIosSend } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 
 const HomePage = () => {
@@ -31,26 +32,29 @@ const HomePage = () => {
         <Grid container spacing={2} style={{ padding: '20px' }}>
             <Grid item xs={12} sm={6}>
             
-                <Card raised>
-                    <CardContent>
-                        <Typography variant="h5" gutterBottom>Add Expenses</Typography>
-                        <CardMedia
-                            component="img"
-                            image={Expenses}
-                            alt="Add Expenses Screenshot"
-                            style={{ height: '200px', objectFit: 'contain' }}
-                            sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.35)' } }}
-                        />
-                        <Typography variant="body1">
-                            Who spent all the money? Oh, it was you. Track your expenses here!
-                        </Typography>
-                    </CardContent>
-                </Card>
+            <Card raised>
+  <CardContent>
+    <Typography variant="h5" gutterBottom>Add Expenses</Typography>
+    <Link to="/expenses">
+      <CardMedia
+        component="img"
+        image={Expenses}
+        alt="Add Expenses Screenshot"
+        style={{ height: '200px', objectFit: 'contain' }}
+        sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.35)' } }}
+      />
+    </Link>
+    <Typography variant="body1">
+      Who spent all the money? Oh, it was you. Track your expenses here!
+    </Typography>
+  </CardContent>
+</Card>
             </Grid>
             <Grid item xs={12} sm={6}>
                 <Card raised>
                     <CardContent>
                         <Typography variant="h5" gutterBottom>Transaction History</Typography>
+                        <Link to="/history">
                         <CardMedia
                             component="img"
                             image={TransactionHistory}
@@ -58,6 +62,7 @@ const HomePage = () => {
                             style={{ height: '200px', objectFit: 'contain' }}
                             sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.45)' } }}
                         />
+                        </Link>
                         <Typography variant="body1">
                         Ever feel like a financial archaeologist? Dig through the ruins of your transaction history and piece together the epic saga of your cash crusade!                        </Typography>
                     </CardContent>
@@ -67,6 +72,7 @@ const HomePage = () => {
                 <Card raised>
                     <CardContent>
                         <Typography variant="h5" gutterBottom>Budget</Typography>
+                        <Link to="/budget">
                         <CardMedia
                             component="img"
                             image={Budget}
@@ -74,6 +80,7 @@ const HomePage = () => {
                             style={{ height: '200px', objectFit: 'contain' }}
                             sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.45)' } }}
                         />
+                        </Link>
                         <Typography variant="body1">
                         Assembling your budget like a fragile house of cards? Place each dollar with care, or brace for the gust of unexpected expenses to bring it all down!                        </Typography>
                     </CardContent>
@@ -83,13 +90,15 @@ const HomePage = () => {
                 <Card raised>
                     <CardContent>
                         <Typography variant="h5" gutterBottom>Budget Tracker</Typography>
-                        <CardMedia
+                       <Link to="/tracking">
+                       <CardMedia
                             component="img"
                             image={BudgetTracker}
                             alt="Budget Tracker Screenshot"
                             style={{ height: '200px', objectFit: 'contain' }}
                             sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.45)' } }}
                         />
+                          </Link>
                         <Typography variant="body1">
                         Think you're a budgeting wizard until reality casts a 'Broke Again' spell? Track your fiscal fantasies vs. harsh truths here and brace for impact!                        </Typography>
                     </CardContent>
@@ -99,6 +108,7 @@ const HomePage = () => {
                 <Card raised>
                     <CardContent>
                         <Typography variant="h5" gutterBottom>Goals</Typography>
+                       <Link to="/goals">
                         <CardMedia
                             component="img"
                             image={Goals}
@@ -106,6 +116,7 @@ const HomePage = () => {
                             style={{ height: '200px', objectFit: 'contain' }}
                             sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.25)' } }}
                         />
+                        </Link>
                         <Typography variant="body1">
                             Saving to hedge against the crippling depression of poverty? Set a goal and track your progress!
                         </Typography>
@@ -116,6 +127,7 @@ const HomePage = () => {
                 <Card raised>
                     <CardContent>
                         <Typography variant="h5" gutterBottom>Statistics</Typography>
+                        <Link to="/statistics">
                         <CardMedia
                             component="img"
                             image={Statistics}
@@ -123,6 +135,7 @@ const HomePage = () => {
                             style={{ height: '200px', objectFit: 'contain' }}
                             sx={{ transition: 'transform 0.15s ease-in-out', '&:hover': { transform: 'scale(1.25)' } }}
                         />
+                        </Link>
                         <Typography variant="body1">
                             View stats over how you spend your money!
                         </Typography>
