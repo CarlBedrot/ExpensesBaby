3eba2511abdaeb0b9e871592e1df2169d45a0c97
CarlBedrot <ca1171be-s@student.lu.se>
2024-01-26 15:11:20 +0100

    test

--- src/components/BudgetComponent.js
+++ src/components/BudgetComponent.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, List, ListItem, ListItemText, Box, Card, CardContent, CardActions, Typography, Select, MenuItem } from '@mui/material';
 
-// hello world 
+// hello world baby
 
 /**
  * Represents a component for managing budgets.
